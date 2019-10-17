e013ba758a6c660082d482a2145f75371e00052a
adamwinzdesign <adamwinzdesign@gmail.com>
2019-10-16 21:11:41 -0500

    resolved issue with the passing of props

--- team-builder/src/components/MemberCard.js
+++ team-builder/src/components/MemberCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MemberCard = props => {
   return (
     <div className = 'member-list'>
-      {props.members.map(member => (
+      {props.memberCard.map(member => (
         <div className = 'member' key= {member.id}>
           <h1>{member.name}</h1>
           <h2>{member.email}</h2>
